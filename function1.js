44f816cb37035cf3733720620c055db8e3f83640
KudakwasheMavuru <43370333+KudakwasheMavuru@users.noreply.github.com>
2022-06-15 10:18:31 +0200

    Add files via upload

---
+++ function1.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Thank you for submitting your details. We will send you an update shortly!!");
+    })
+  });
+});
